d0e366eb669f32c172b3536ecbb24298ceab05f3
MeiirzhanBalta <141903078+MeiirzhanBalta@users.noreply.github.com>
2024-01-21 00:11:30 +0600

    Meiirzhan Baltabayev

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -9,7 +9,7 @@ namespace WindowsFormsApp1
     public partial class Form1 : Form
     {
         private string chromeDriverPath = "chromedriver.exe";
-        private IWebDriver driver; // Добавили поле для хранения экземпляра браузера
+        private IWebDriver driver; 
 
         public Form1()
         {
@@ -19,31 +19,21 @@ namespace WindowsFormsApp1
         private void button1_Click(object sender, EventArgs e)
         {
             string searchTerm = txtSerch.Text;
-
-            string absolutePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, chromeDriverPath);
-
-            // Инициализация браузера
+            string absolutePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, chromeDriverPath);        
             driver = new ChromeDriver(absolutePath);
-
             driver.Navigate().GoToUrl("https://www.ebay.com/");
-
             var searchField = driver.FindElement(By.Id("gh-ac"));
             searchField.SendKeys(searchTerm);
-
             var searchButton = driver.FindElement(By.Id("gh-btn"));
             searchButton.Click();
-
             string searchResultUrl = driver.Url;
-
-            lstSearchResults.Items.Add(searchResultUrl);
-
-            // Если нужно видеть результат, держи консольное окно открытым
+            lstSearchResults.Items.Add(searchResultUrl);         
             Console.WriteLine($"Результат поиска: {searchResultUrl}");
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            // Закрытие браузера
+            
             if (driver != null)
             {
                 driver.Quit();
@@ -60,8 +50,6 @@ namespace WindowsFormsApp1
             if (driver != null)
             {
                 driver.Navigate().Back();
-
-                // Очистка полей
                 txtSerch.Text = "";
                 lstSearchResults.Items.Clear();
             }
